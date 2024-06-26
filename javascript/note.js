1c8f6be419cf1f9f6010015896640b8e66ddccda
samedy-leader <fordancestyle@gmail.com>
2024-06-26 15:49:11 +0700

    feature/#2: addition of navbar component

---
+++ javascript/note.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const navBarElementLeft = [
+    {logo : './assets/svg/home.svg', class : 'homepage'},
+    {logo : './assets/svg/america.svg', class : 'right'},
+    {logo : './assets/svg/german.svg', class : 'right'},
+    {logo : './assets/svg/cambodian.svg', class : 'right'},
+    {logo : './assets/svg/thai.svg', class : 'right'},
+]
+
