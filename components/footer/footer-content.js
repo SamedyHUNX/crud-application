41e238171e02c96c427980e81f1e7a0c8b6f4640
samedy-leader <fordancestyle@gmail.com>
2024-07-02 10:25:57 +0700

    feature/#5: suceed in adding the footer elements within it

--- components/footer/footer-content.js
+++ components/footer/footer-content.js
@@ -1,10 +1,13 @@
 'use strict';
 
+// array to store footer content
 const footerContent = [
     {text : 'HI'},
-    {svg : ['./assets/svg/youtube.svg', './assets/svg/gitlab.svg', './assets/svg/github.svg']},
+    {svg : ['./assets/svg/youtube.svg', './assets/svg/gitlab.svg', './assets/svg/github.svg', './assets/svg/notion.svg']},
+    {link : ['youtube', 'https://git.clp.kr/anbschool/2nd/luck/hun-vadhna-samedy/personal_diary_project.git', 'https://github.com/SamedyHUNX/crud-application']}
 ]
 
+// dynamically create element and add based on the array
 const footerContainer = document.querySelector('.footer-container');
 const footerTextDiv = footerContainer.querySelector('.footer-text');
 const footerLogosDiv = footerContainer.querySelector('.footer-logos');
@@ -13,8 +16,17 @@ footerLogosDiv.classList.add('right');
 
 footerTextDiv.textContent = footerContent[0].text;
 
-footerContent[1].svg.forEach(svgPath => {
+footerContent[1].svg.forEach((svgPath, index) => {
+    const a = document.createElement('a');
     const img = document.createElement('img');
     img.src = svgPath;
-    footerLogosDiv.appendChild(img);
+
+    // add link (href) from the footerContent array
+    if (footerContent[2] && footerContent[2].link[index]) {
+        a.href = footerContent[2].link[index];
+        a.target = '_blank';
+    }
+
+    a.appendChild(img);
+    footerLogosDiv.appendChild(a);
 })
